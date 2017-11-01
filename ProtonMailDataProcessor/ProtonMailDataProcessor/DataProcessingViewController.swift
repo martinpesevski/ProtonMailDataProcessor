6783f5ad91a04b64a16c07b98339e1d58024fd14
Martin Peshevski <martin.m.pesevski@gmail.com>
2017-11-01 08:18:12 +0100

    added screens

--- ProtonMailDataProcessor/ProtonMailDataProcessor/DataProcessingViewController.swift
+++ ProtonMailDataProcessor/ProtonMailDataProcessor/DataProcessingViewController.swift
@@ -8,18 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class DataProcessingViewController: UIViewController {
 
+    @IBOutlet var pendingCompletedSegmentedControl: UISegmentedControl!
+    @IBOutlet var dataTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
